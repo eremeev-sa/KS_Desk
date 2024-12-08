5f09c4e7694bb196ff543966d22e15a6885fc697
SergeyEremeev <Eremeevsa2003@mail.ru>
2024-12-08 11:44:22 +0300

    2

--- backend/Model/KanbanColumn.cs
+++
@@ -1,5 +0,0 @@
-public class KanbanColumn {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public List<Task> Tasks { get; set; }
-}
--- backend/Model/Task.cs
+++
@@ -1,7 +0,0 @@
-public class Task {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Assignee { get; set; }
-    public int Priority { get; set; }
-    public string Description { get; set;}
-}
